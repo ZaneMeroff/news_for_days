a150702a1461db5117a0f74755926acdc0a0dbfc
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-27 18:13:25 -0700

    Add news container component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle.js';
+import App from '../App/App.js';
+import './NewsContainer.css';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = prop => {
+  return (
+    <div className='news-container'>
+      {
+        prop.localNews.map(article => (
+        <NewsArticle
+          image={article.img}
+          headline={article.headline}
+          description={article.description}
+        />
+        ))
+      }
+    </div>
+  )
+}
 
 export default NewsContainer;
