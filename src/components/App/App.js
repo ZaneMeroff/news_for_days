004f875fc05e1c878381e2179184c275fc483869
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-28 22:50:10 -0700

    Change state value

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import NewsArticle from '../NewsArticle/NewsArticle';
 import Menu from '../Menu/Menu';
-// import SearchForm from '../SearchForm/SearchForm';
 import local from '../../data/local';
+import health from '../../data/health';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      local
-    }
+    this.state = {data: local}
   }
 
-  render () {
+  setNewsType(newsType) {
+    this.setState( {data: newsType} );
+  }
+
+  render() {
     return (
       <div className="app">
-        <Menu />
+        <Menu setNewsType={this.setNewsType}/>
         <NewsContainer localNews={local}/>
       </div>
     );
