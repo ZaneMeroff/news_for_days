099bc96867d8e1e1b61c11c6dbf6b66d53612b30
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-29 18:55:45 -0700

    Add search funtionality

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
-  return (
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  onSearchChange = event => {
+    this.setState({query: event.target.value})
+  }
+
+  render() {
+    return (
     <section className='search-form-container'>
-     <input className='search-input' placeholder='search for articles here...'></input>
-     <button className='search-button'>search</button>
+     <input className='search-input' onChange={this.onSearchChange} placeholder='search for articles here...'></input>
+     <button onClick={() => this.props.filterSearchResults(this.state.query)} className='search-button'>search</button>
     </section>
-  )
+    )
+  }
 
 }
 
