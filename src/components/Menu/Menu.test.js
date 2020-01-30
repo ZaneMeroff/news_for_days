b598611de214d9bea59a8b81b1143d2e7f16ec15
Zachary Nemeroff <zanemeroff@gmail.com>
2020-01-30 16:52:12 -0700

    Add tests

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -1,3 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Menu from './Menu'
 import { shallow } from 'enzyme';
+
+describe('Menu', () => {
+
+  it('should be able to render menu to page', () => {
+    const wrapper = shallow(<Menu
+      setNewsType={jest.fn()}
+      selectedNewsType={'local'}
+      />)
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('expect selectNewsType to be called on button click', () => {
+    const mockSetNewsType = jest.fn();
+    const wrapper = shallow(<Menu
+      setNewsType={mockSetNewsType}
+      selectedNewsType={'local'}
+      />)
+    wrapper.find('button').at(0).simulate('click')
+    expect(mockSetNewsType).toHaveBeenCalled();
+  });
+
+});
